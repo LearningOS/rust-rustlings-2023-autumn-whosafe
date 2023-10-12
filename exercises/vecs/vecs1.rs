e316d005ce2ff3b15f4eb2b6d09be64e76774804
whosafe <zenghongl@126.com>
2023-10-12 22:37:21 +0800

    day 2

--- exercises/vecs/vecs1.rs
+++ exercises/vecs/vecs1.rs
@@ -7,12 +7,12 @@
 //
 // Execute `rustlings hint vecs1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
-    let v = // TODO: declare your vector here with the macro for vectors
-
+    // let v = [10, 20, 30, 40].to_vec();// TODO: declare your vector here with the macro for vectors
+    let mut  v: Vec<i32> = Vec::new();
+    v = vec![10, 20, 30, 40];
     (a, v)
 }
 
@@ -23,6 +23,6 @@ mod tests {
     #[test]
     fn test_array_and_vec_similarity() {
         let (a, v) = array_and_vec();
-        assert_eq!(a, v[..]);
+        assert_eq!(a.to_vec(), v[..]);
     }
 }
